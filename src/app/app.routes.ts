a242c1dcbb8661ff75c35328983b2712562c3f14
Bahri.Busch <turkbahri19@hotmail.com>
2024-01-01 20:00:06 +0300

    project structure changed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { TestComponent } from '../components/test/test.component';
+import { AlghoritmsFirstWeekComponent } from '../components/alghoritms-first-week/alghoritms-first-week.component';
+import { AlghoritmsDoorComponent } from '../components/alghoritms-door/alghoritms-door.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"alghoritmsfirstweek"  ,component:AlghoritmsFirstWeekComponent},
+    {path:"test"  ,component:TestComponent},
+    {path:"alghoritmsdoor",component:AlghoritmsDoorComponent}
+];
